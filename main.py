4206ace06bc32a838835dfbf44dc7798ff7d155e
kehach07 <kehachandrakar07@gmail.com>
2026-07-04 23:47:33 +0530

    Deploy OAuth verifier

--- main.py
+++ main.py
@@ -2,27 +2,35 @@ from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 import jwt
+from cryptography.hazmat.primitives import serialization
 
 app = FastAPI()
 
 ISSUER = "https://idp.exam.local"
 AUDIENCE = "tds-gpqe27p0.apps.exam.local"
 
-PUBLIC_KEY = b"""-----BEGIN PUBLIC KEY-----
+PUBLIC_KEY_PEM = b"""-----BEGIN PUBLIC KEY-----
 MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA2okOHspNjgA+2rTLbeuY
 cxiP/hG8C6Sb9iwg3yiLAA4HCnpITcbWCSelbvbYGuc3EbNy4xFyf5Cbj5DHJMID
 EkryOgyd2giIIIBOUBj8S63uGcnRpOBh9NFatfNwheKuzsPuVNldu6A9cNteNpXc
 WyJjG2axVfmq7i6SuKr1JoWYG7xTTAvKPujSl4OtsQfO3h5NepzdfXpr28oNnzfW
 ed+zclR6BcmNNo/WVfJ4xyCLSf0BCOgdTgW6PdaChd1l9VDetJZVEgC5tkyvXsfI
-SI6iyrYbKR0NEBSqq4XkadEjsCs4LlgniT7GlkL9Mce3b0wGLs9/7ZIXdQIDAQAB
+SI6iyrYbKR0NEBSqq4XkadEjsCs4LlgniT7GlkL9Mce3b0wGLs9/7ZIX
+dQIDAQAB
 -----END PUBLIC KEY-----"""
+
+PUBLIC_KEY = serialization.load_pem_public_key(PUBLIC_KEY_PEM)
+
 
 class TokenRequest(BaseModel):
     token: str
 
+
 @app.get("/")
 def root():
     return {"message": "OAuth Token Verification Service is running"}
+
+
 @app.post("/verify")
 def verify(request: TokenRequest):
     try:
@@ -42,5 +50,8 @@ def verify(request: TokenRequest):
         }
 
     except Exception as e:
-        print("JWT ERROR:", repr(e))
-        raise
+        print(type(e).__name__, str(e))
+        return JSONResponse(
+            status_code=401,
+            content={"valid": False},
+        )
